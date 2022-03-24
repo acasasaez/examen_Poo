048fe205cdb7149cdd9ae421111027cd81c30fd7
acasasaez <acasasae@myuax.com>
2022-03-24 08:54:53 +0100

    Creación de setters and getters: Métodos de acceso a los atributos de una clase

--- ejercicio3.py/CuentaBancaria.py
+++ ejercicio3.py/CuentaBancaria.py
@@ -9,3 +9,35 @@ class CuentaBancaria():
         self.fecha=fecha #self.fecha toma el valor de fecha
         self.numerocuenta=numerocuenta #self.numerocuenta toma el valor de numerocuenta
         self.saldo=saldo #self.saldo toma el valor de saldo
+#Creación de setters and getters: Métodos de acceso a los atributos de una clase
+
+#Creamos los "set" que nos permiten modificar el valor de su respectivo atributo
+    def setid(self,id):
+        self.id=id
+
+    def settitular(self,titular):
+        self.titular=titular
+
+    def setFecha(self,fecha):
+        self.fecha=fecha
+
+    def setnumerocuenta(self,numerocuenta):
+        self.numerocuenta=numerocuenta
+
+    def setsaldo(self,saldo):
+        self.saldo=saldo
+#Creamos los "get" que nos permiten mostrar el valor de su respectivo atributo
+    def getid(self):
+        return self.id
+
+    def gettitular(self):
+        return self.titular
+
+    def getFecha(self):
+        return self.fecha
+
+    def getnumerocuenta(self):
+        return self.numerocuenta
+
+    def getsaldo(self):
+        return self.saldo
